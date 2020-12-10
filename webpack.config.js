c6c85a16d3e3e661ad57acb82f6afa93ed67fdf2
Aaron Faanes <dafrito@gmail.com>
2020-12-09 22:44:00 -0600

    Change to generateID

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   entry: path.resolve(__dirname, "src/index.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "TODO-PACKAGE-NAME.js",
+    filename: "generateID.js",
     globalObject: "this",
-    library: "getTimeInMillis",
+    library: "generateID",
     libraryTarget: "umd",
   },
   module: {
